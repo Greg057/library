60d1aaae6ff23998fb348836f4de1664c1132952
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-10 19:43:17 +0100

    Fix bugs + book card UI

--- logic.js
+++ logic.js
@@ -9,6 +9,7 @@ const authorForm = document.querySelector("#author");
 const pagesForm = document.querySelector("#pages");
 const isReadForm = document.querySelector("#isRead");
 const submitBtn = document.querySelector("#submit-button");
+const form = document.querySelector("form");
 
 const myLibrary = [];
 
@@ -24,27 +25,30 @@ submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
     myLibrary.push(new Book(titleForm.value, authorForm.value, pagesForm.value, isReadForm.checked));
     dialog.close();
+    form.reset();
     displayBooks();
 })
 
 function displayBooks () {
-    myLibrary.forEach(function (book) {
-        if(book["isRead"] == true) {
-            isRead = "Read";
-        } else {
-            isRead = "Not Read";
-        }
-        bookList.innerHTML +=  `<div class="book-card">
-                                    <div class="big-text">Title</div>
-                                    <div>${book["title"]}</div>
-                                    <div class="big-text">Author</div>
-                                    <div>${book["author"]}</div>
-                                    <div class="big-text">Number of Pages</div>
-                                    <div>${book["pages"]}</div>
-                                    <button id="book-read">${isRead}</button>
-                                    <button id="remove-book">Remove</button>
-                                </div>`
-    })
+    const book = myLibrary[myLibrary.length - 1];
+    if(book["isRead"] == true) {
+        isRead = "Read";
+        id = "book-is-read";
+    } else {
+        isRead = "Not Read";
+        id = "book-not-read";
+    }
+    bookList.innerHTML +=  `<div class="book-card">
+                                <div class="big-text">Title</div>
+                                <div>${book["title"]}</div>
+                                <div class="big-text">Author</div>
+                                <div>${book["author"]}</div>
+                                <div class="big-text">Pages</div>
+                                <div>${book["pages"]}</div>
+                                <button class="book-read" id=${id}>${isRead}</button>
+                                <button id="remove-book">Remove</button>
+                            </div>`
+    
 }
 
 
